fa01d75c90429069f4e92d9889032bae49c5c869
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2021-12-09 21:41:19 +0100

    Update converto.sh

--- converto.sh
+++ converto.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #This is converto: a batch converter [from svg to png] using inline inkscape, but in a easier way. For this to work, you'll need to install inkscape (that is the BEST free vector graphics software) [FULL INSTALLATION, NOT MINIMAL] https://inkscape.org/release/inkscape-1.1.1/. This script only works on Inkscape version 1.0 and above.
 echo -e "\e[40;38;5;220mIMPORTANT: Before going on, make sure that in this directory there aren't any PNG files, otherwise they will be moved with the converted files into another path.\e[0m"
 read -p "Are you sure to continue? [Y/n]" -n 1 -r
